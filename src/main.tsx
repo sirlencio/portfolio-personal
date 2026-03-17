107062d57f046a481ce6a31518491189c23f53e2
Sirlencio <carlos.l.p.251001@gmail.com>
2026-03-17 16:45:40 +0100

    fix import

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import MainPage from './pages/MainPage'
-import { Analytics } from "@vercel/analytics/next"
+import { Analytics } from '@vercel/analytics/react'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <MainPage />
     <Analytics />
+    <MainPage />
   </StrictMode>,
 )
